61def7b54cf49d1500374adcc7a5c4ba76797731
thepetk <thepetk@gmail.com>
2025-10-12 14:14:14 +0100

    Add entry point

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# parse inputs from action.yml
+DYNAMIC_PLUGINS_PATH="$1"
+GITHUB_TOKEN="$2"
+PLUGIN_FILTER="$3"
+PR_MODE="$4"
+MAX_PRS="$5"
+
+echo "Starting RHDH Plugin GitOps Updater..."
+echo "Dynamic plugins path: $DYNAMIC_PLUGINS_PATH"
+echo "PR mode: $PR_MODE"
+echo "Max PRs: $MAX_PRS"
+
+uv run python hello.py \
+  --dynamic-plugins-path "$DYNAMIC_PLUGINS_PATH" \
+  --github-token "$GITHUB_TOKEN" \
+  --plugin-filter "$PLUGIN_FILTER" \
+  --pr-mode "$PR_MODE" \
+  --max-prs "$MAX_PRS"
